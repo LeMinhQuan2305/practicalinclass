d51001ba3a49cc64e02459b1ca0d453e4c7cf393
Lê Minh Quân <56839807+LeMinhQuan2305@users.noreply.github.com>
2024-03-12 14:59:42 +0700

    Update Q2.py

--- Q2.py
+++ Q2.py
@@ -1,37 +1,20 @@
-class Main:
-    def f1(self, inputString):
-        a = inputString.split(', ')
-        for x in a:
-            print(x)
-    def f2(self, inputString):
-        import math
-        x = 0
-        y = 0
-        a = inputString.split(', ')
-        for item in a:
-            lst = item.split()
-            direction, distance = lst
-            if direction == "UP":
-                x += int(distance)
-            elif direction == "DOWN":
-                x -= int(distance)
-            elif direction == "LEFT":
-                y -= int(distance)
-            elif direction == "RIGHT":
-                y += int(distance)
-        res = math.sqrt(x**2 + y**2)
-        print(round(res,2))
+class main:
+    def GuessNumber(self, number):
+        a = 0
+        b = int(number)
+        state = ''
+        while state != 'c':
+            comp = random.randint(a,b)
+            if state == 'h':
+                b = comp -1
+            elif(state == '1'):
+                a = comp + 1
+        print('Well done! The computer guessed your number  ' + str(comp) + ' correctly')
+        
     def main(self):
-        print(" 1. Test f1 (2 mark)")
-        print(" 2. Test f2 (1 mark)")
-        choice = int(input())
-        inputString = input()
-        if choice == 1:
-            self.f1(inputString)
-        elif choice == 2:
-            self.f2(inputString)
-        else:
-            print("Wrong select")
-        print("FINISH")
+        number = input('Enter a range for guessed number: ')
+        print('OUTPUT')
+        self.GuessNumber(number)
+    print("FINISH")
 main = Main()
 main.main()
